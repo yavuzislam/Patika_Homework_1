f95ca5c0859e7497080092757434f3daed8ec8d8
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2023-12-20 22:54:05 +0300

    Create CreateCustomerCommand.cs

---
+++ Customer_management/Customer_management/CustomerOperations/Command/CreateCustomer/CreateCustomerCommand.cs
@@ -0,0 +1,48 @@
+﻿using Customer_management.DbOperation;
+using Customer_management.Entities;
+
+namespace Customer_management.CustomerOperations.Command.CreateCustomer;
+
+public class CreateCustomerCommand
+{
+    private readonly CustomerManagementDbContext _context;
+    public CreateCustomerModel Model { get; set; }
+
+    public CreateCustomerCommand(CustomerManagementDbContext context)
+    {
+        _context = context;
+    }
+
+    public void Handle()
+    {
+        var customer = _context.Customers.SingleOrDefault(x => x.Job == Model.Job);
+        if (customer is not null)
+        {
+            throw new InvalidOperationException("Customer already exists");
+        }
+
+        customer = new Customer
+        {
+            Name = Model.Name,
+            Surname = Model.Surname,
+            BirthDate = Model.BirthDate.Date,
+            Job = Model.Job,
+            Country = Model.Country,
+            IncomeLevelId = Model.IncomeLevelId,
+            MartialStatusId = Model.MartialStatusId
+        };
+        _context.Customers.Add(customer);
+        _context.SaveChanges();
+    }
+}
+
+public class CreateCustomerModel
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public DateTime BirthDate { get; set; }
+    public string Job { get; set; }
+    public string Country { get; set; }
+    public int IncomeLevelId { get; set; }
+    public int MartialStatusId { get; set; }
+}
