b8cd61e6b6d890b3dcbf90efbd199bd6f20eb34d
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2023-12-20 22:54:26 +0300

    Create Customer.cs

---
+++ Customer_management/Customer_management/Entities/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Customer_management.Entities;
+
+public class Customer
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public DateTime BirthDate { get; set; }
+    public string Job { get; set; }
+    public string Country { get; set; }
+    public int IncomeLevelId { get; set; }
+    public int MartialStatusId { get; set; }
+}
