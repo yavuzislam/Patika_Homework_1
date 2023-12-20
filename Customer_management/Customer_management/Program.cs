c566b733acda56457a2d2851eaf1df81f7c30e80
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2023-12-20 16:45:13 +0300

    init

---
+++ Customer_management/Customer_management/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Customer_management;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var host = Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>()).Build();
+        host.Run();
+    }
+}
---
+++ Customer_management/Customer_management/Startup.cs
@@ -0,0 +1,36 @@
+﻿namespace Customer_management;
+
+public class Startup
+{
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+    }
+}
