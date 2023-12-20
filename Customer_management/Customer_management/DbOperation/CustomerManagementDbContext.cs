e4957e0991a423f4aeebe50626d3a20e14991f90
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2023-12-20 22:54:22 +0300

    Create CustomerManagementDbContext.cs

---
+++ Customer_management/Customer_management/DbOperation/CustomerManagementDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Customer_management.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Customer_management.DbOperation;
+
+public class CustomerManagementDbContext : DbContext
+{
+    public CustomerManagementDbContext(DbContextOptions<CustomerManagementDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Customer> Customers { get; set; }
+}
