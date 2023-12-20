6c6b76787e8f2d0a60a71423991727896fd504ef
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2023-12-20 22:54:10 +0300

    Create DeleteCustomerCommand.cs

---
+++ Customer_management/Customer_management/CustomerOperations/Command/DeleteCustomer/DeleteCustomerCommand.cs
@@ -0,0 +1,26 @@
+﻿using Customer_management.DbOperation;
+
+namespace Customer_management.CustomerOperations.Command.DeleteCustomer;
+
+public class DeleteCustomerCommand
+{
+    private readonly CustomerManagementDbContext _context;
+    public int CustomerId { get; set; }
+
+    public DeleteCustomerCommand(CustomerManagementDbContext context)
+    {
+        _context = context;
+    }
+
+    public void Handle()
+    {
+        var customer = _context.Customers.SingleOrDefault(x => x.Id == CustomerId);
+        if (customer is null)
+        {
+            throw new InvalidOperationException("Customer not found");
+        }
+
+        _context.Customers.Remove(customer);
+        _context.SaveChanges();
+    }
+}
