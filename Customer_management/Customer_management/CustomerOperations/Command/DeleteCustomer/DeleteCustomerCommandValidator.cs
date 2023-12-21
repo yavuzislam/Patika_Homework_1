85e70a929863e4c2d31b823e1c0f4e478ed86326
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2023-12-21 15:06:29 +0300

    Create DeleteCustomerCommandValidator.cs

---
+++ Customer_management/Customer_management/CustomerOperations/Command/DeleteCustomer/DeleteCustomerCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace Customer_management.CustomerOperations.Command.DeleteCustomer;
+
+public class DeleteCustomerCommandValidator : AbstractValidator<DeleteCustomerCommand>
+{
+    public DeleteCustomerCommandValidator()
+    {
+        RuleFor(x => x.CustomerId).GreaterThan(0);
+    }
+}
