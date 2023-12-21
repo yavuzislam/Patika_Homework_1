3c837da401eec1848971cf2969e5854e53393ae8
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2023-12-21 15:06:23 +0300

    Update CreateCustomerCommand.cs

--- Customer_management/Customer_management/CustomerOperations/Command/CreateCustomer/CreateCustomerCommand.cs
+++ Customer_management/Customer_management/CustomerOperations/Command/CreateCustomer/CreateCustomerCommand.cs
@@ -1,4 +1,5 @@
-﻿using Customer_management.DbOperation;
+﻿using AutoMapper;
+using Customer_management.DbOperation;
 using Customer_management.Entities;
 
 namespace Customer_management.CustomerOperations.Command.CreateCustomer;
@@ -6,11 +7,13 @@ namespace Customer_management.CustomerOperations.Command.CreateCustomer;
 public class CreateCustomerCommand
 {
     private readonly CustomerManagementDbContext _context;
+    private readonly IMapper _mapper;
     public CreateCustomerModel Model { get; set; }
 
-    public CreateCustomerCommand(CustomerManagementDbContext context)
+    public CreateCustomerCommand(CustomerManagementDbContext context, IMapper mapper)
     {
         _context = context;
+        _mapper = mapper;
     }
 
     public void Handle()
@@ -21,16 +24,18 @@ public class CreateCustomerCommand
             throw new InvalidOperationException("Customer already exists");
         }
 
-        customer = new Customer
-        {
-            Name = Model.Name,
-            Surname = Model.Surname,
-            BirthDate = Model.BirthDate.Date,
-            Job = Model.Job,
-            Country = Model.Country,
-            IncomeLevelId = Model.IncomeLevelId,
-            MartialStatusId = Model.MartialStatusId
-        };
+        customer = _mapper.Map<Customer>(Model);
+
+        // customer = new Customer
+        // {
+        //     Name = Model.Name,
+        //     Surname = Model.Surname,
+        //     BirthDate = Model.BirthDate.Date,
+        //     Job = Model.Job,
+        //     Country = Model.Country,
+        //     IncomeLevelId = Model.IncomeLevelId,
+        //     MartialStatusId = Model.MartialStatusId
+        // };
         _context.Customers.Add(customer);
         _context.SaveChanges();
     }
