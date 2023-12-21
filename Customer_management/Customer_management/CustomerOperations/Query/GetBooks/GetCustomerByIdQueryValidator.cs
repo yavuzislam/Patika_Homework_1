c4d50b891c79b7a6ff6716df9d2706492712674d
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2023-12-21 15:06:43 +0300

    Create GetCustomerByIdQueryValidator.cs

---
+++ Customer_management/Customer_management/CustomerOperations/Query/GetBooks/GetCustomerByIdQueryValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace Customer_management.CustomerOperations.Query.GetBooks;
+
+public class GetCustomerByIdQueryValidator : AbstractValidator<GetCustomersByIdQuery>
+{
+    public GetCustomerByIdQueryValidator()
+    {
+        RuleFor(x => x.CustomerId).GreaterThan(0);
+    }
+}
