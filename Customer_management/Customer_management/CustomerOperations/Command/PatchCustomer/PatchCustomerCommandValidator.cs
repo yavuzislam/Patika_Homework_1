2607bfb111ab9663387ba9df45c1709aff3128ca
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2023-12-21 15:06:35 +0300

    Create PatchCustomerCommandValidator.cs

---
+++ Customer_management/Customer_management/CustomerOperations/Command/PatchCustomer/PatchCustomerCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.JsonPatch;
+
+namespace Customer_management.CustomerOperations.Command.PatchCustomer;
+
+public class PatchCustomerCommandValidator : AbstractValidator<PatchCustomerCommand>
+{
+    public PatchCustomerCommandValidator()
+    {
+        RuleFor(x => x.CustomerId).GreaterThan(0);
+        RuleFor(x => x.PatchDocument).Must(ContainOnlyJobOperation)
+            .WithMessage("Only 'Job' can be updated.");
+    }
+
+    private bool ContainOnlyJobOperation(JsonPatchDocument<PatchCustomerModel> patchDoc)
+    {
+        if (patchDoc != null && patchDoc.Operations.Count == 1)
+        {
+            var operation = patchDoc.Operations[0];
+            return operation.path == "/job";
+        }
+
+        return false;
+    }
+}
