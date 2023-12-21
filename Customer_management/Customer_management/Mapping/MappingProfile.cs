d586f86db8c2e888f9ec147e45978c1194e253cd
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2023-12-21 15:06:51 +0300

    Create MappingProfile.cs

---
+++ Customer_management/Customer_management/Mapping/MappingProfile.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Customer_management.Common;
+using Customer_management.CustomerOperations.Command.CreateCustomer;
+using Customer_management.CustomerOperations.Command.PatchCustomer;
+using Customer_management.CustomerOperations.Command.UpdateCustomer;
+using Customer_management.CustomerOperations.Query.GetBooks;
+using Customer_management.Entities;
+using Microsoft.AspNetCore.JsonPatch;
+
+namespace Customer_management.Mapping;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Customer, CustomersViewModel>().ForMember(dest => dest.FullName,
+                opt => opt.MapFrom(src => src.Name + " " + src.Surname))
+            .ForMember(dest => dest.BirthDate, opt => opt.MapFrom(src => src.BirthDate.Date.ToString("dd/MM/yyyy")))
+            .ForMember(dest => dest.IncomeLevel,
+                opt => opt.MapFrom(src => ((IncomeLevelType)src.IncomeLevelId).ToString()))
+            .ForMember(dest => dest.MartialStatus,
+                opt => opt.MapFrom(src => ((MartialStatusType)src.MartialStatusId).ToString()));
+
+        CreateMap<Customer, GetCustomerViewModel>().ForMember(dest => dest.FullName,
+                opt => opt.MapFrom(src => src.Name + " " + src.Surname))
+            .ForMember(dest => dest.BirthDate, opt => opt.MapFrom(src => src.BirthDate.Date.ToString("dd/MM/yyyy")))
+            .ForMember(dest => dest.IncomeLevel,
+                opt => opt.MapFrom(src => ((IncomeLevelType)src.IncomeLevelId).ToString()))
+            .ForMember(dest => dest.MartialStatus,
+                opt => opt.MapFrom(src => ((MartialStatusType)src.MartialStatusId).ToString()));
+
+        CreateMap<CreateCustomerModel, Customer>()
+            .ForMember(dest => dest.BirthDate, opt => opt.MapFrom(src => src.BirthDate.Date));
+
+        CreateMap<UpdateCustomerModel, Customer>().ForMember(dest => dest.IncomeLevelId,
+            opt => opt.MapFrom(src => ((IncomeLevelType)src.IncomeLevelId).ToString())).ForMember(
+            dest => dest.MartialStatusId,
+            opt => opt.MapFrom(src => ((MartialStatusType)src.MartialStatusId).ToString()));
+
+        CreateMap<JsonPatchDocument<PatchCustomerModel>, PatchCustomerModel>().ForAllMembers(opt => opt.Ignore());
+    }
+}
