9f7561a00ef3f31371dda5a9c0e74ed73b68645f
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2023-12-21 15:06:41 +0300

    Create UpdateCustomerCommandValidator.cs

---
+++ Customer_management/Customer_management/CustomerOperations/Command/UpdateCustomer/UpdateCustomerCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Customer_management.CustomerOperations.Command.UpdateCustomer;
+
+public class UpdateCustomerCommandValidator : AbstractValidator<UpdateCustomerCommand>
+{
+    public UpdateCustomerCommandValidator()
+    {
+        RuleFor(x => x.CustomerId).GreaterThan(0);
+        RuleFor(x => x.Model.Name).NotEmpty().MinimumLength(3);
+        RuleFor(x => x.Model.Surname).NotEmpty().MinimumLength(3);
+        RuleFor(x => x.Model.Job).NotEmpty().MinimumLength(3);
+        RuleFor(x => x.Model.IncomeLevelId).NotEmpty().GreaterThan(0).LessThan(4);
+        RuleFor(x => x.Model.MartialStatusId).NotEmpty().GreaterThan(0).LessThan(5);
+    }
+}
